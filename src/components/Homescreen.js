e9651a4800980dbcc95e183762a4f339c3c1ce7e
Aliaksandr Lubneuski <aliaksandrlubneuski@Aliaksandrs-MacBook-Air.local>
2022-11-10 10:29:37 +0100

    feature: category picker

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -6,65 +6,6 @@ import SectionedMultiSelect from 'react-native-sectioned-multi-select';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function HomeScreen({ navigation }) {
-  const items = [
-    {  
-      name: "Fruits",
-      id: 0,
-      children: [{
-          name: "Apple",
-          id: 10,
-        },{
-          name: "Strawberry",
-          id: 17,
-        },{
-          name: "Pineapple",
-          id: 13,
-        },{
-          name: "Banana",
-          id: 14,
-        },{
-          name: "Watermelon",
-          id: 15,
-        },{
-          name: "Kiwi fruit",
-          id: 16,
-        }]
-    },
-    {
-      name: "Gems",
-      id: 1,
-      children: [{
-          name: "Quartz",
-          id: 20,
-        },{
-          name: "Zircon",
-          id: 21,
-        },{
-          name: "Sapphire",
-          id: 22,
-        },{
-          name: "Topaz",
-          id: 23,
-        }]
-    },
-    {
-      name: "Plants",
-      id: 2,
-      children: [{
-          name: "Mother In Law\'s Tongue",
-          id: 30,
-        },{
-          name: "Yucca",
-          id: 31,
-        },{
-          name: "Monsteria",
-          id: 32,
-        },{
-          name: "Palm",
-          id: 33,
-        }]
-    },
-  ]
 
   const  handleRequest = () => {
     console.log(selectedItem);
